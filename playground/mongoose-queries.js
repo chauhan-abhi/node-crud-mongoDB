1d6d9a1cec63b2669b4425ac0928b5cb3fe98ae6
chauhan-abhi <chauhanabhi1245@gmail.com>
2018-09-30 13:59:48 +0530

    add GET /todos/:id

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -2,7 +2,7 @@ const {ObjectID} =  require('mongodb')
 
 const {mongoose} = require('./../server/db/mongoose')
 const {Todo} = require('./../server/models/todo')
-const {User} = require('./..server/models/user')
+const {User} = require('./../server/models/user')
 
 var id = '5bae0fa740ceda34c41235a8'
 if(!ObjectID.isValid(id)) {
@@ -21,12 +21,21 @@ if(!ObjectID.isValid(id)) {
 //     console.log('Todo', todo)
 // })
 
-Todo.findById(id).then((todo) => {
-    if(!todo) {
-        return console.log('Id not found')
-    }
-    console.log('Todo by id', todo)
-}).catch((e) => {
-  console.log(e)    
-})
+// Todo.findById(id).then((todo) => {
+//     if(!todo) {
+//         return console.log('Id not found')
+//     }
+//     console.log('Todo by id', todo)
+// }).catch((e) => {
+//   console.log(e)    
+// })
 
+
+User.findById('5bae1ec20080fe3d2480fc93').then((user) => {
+    if(!user) {
+        console.log('Unable to find user')
+    }
+    console.log(JSON.stringify(user, undefined, 2))
+}, (e) => {
+    console.log(e)
+})
--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ var bodyParser = require('body-parser')
 var {mongoose} = require('./db/mongoose')
 var {Todo} = require('./models/todo')
 var {User} = require('./models/user')
+const {ObjectID} =  require('mongodb')
+
 
 var app = express()
 
@@ -29,6 +31,24 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e)
     })
+})
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id
+    // validate id using isValid
+    if(!ObjectID.isValid(id)) {
+        res.status(404 ).send()
+    } else {
+        Todo.findById(id).then((todo) => {
+            if(todo) {
+                res.send({ todo})
+            } else {
+                res.status(404).send()
+            }
+        }, (e) => {
+            res.status(400).send()  
+        })
+    }
 })
 
 app.listen(3000, () => {
